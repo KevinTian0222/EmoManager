b9d2a08fc8efdab4b3f1c58b1f3320e9e02edacc
lida <lida@cmhi.chinamobile.com>
2018-06-25 10:22:58 +0800

    Initial Commit

---
+++ MuseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MuseDemo
+//
+//  Created by Mercedes on 2018/6/25.
+//  Copyright © 2018年 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
